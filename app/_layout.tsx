dc1075b8360b65d42d750377b4a9ca0ee1e9bc7c
JakubLobos <saltver2@onet.eu>
2026-04-08 17:51:32 +0200

    update dependencies, fix project configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,36 @@
-import { useFonts } from 'expo-font';
-import "../global.css"
-import { Slot } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import { AuthProvider } from "@/modules/auth/context/ctx";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import React from "react";
+import "../global.css";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [loaded] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    });
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    return (
-        <AuthProvider>
-            <SafeAreaProvider>
-                <GestureHandlerRootView style={{ flex: 1}}>
-                    <Slot />
-                </GestureHandlerRootView>
-            </SafeAreaProvider>
-        </AuthProvider>
-    );
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <AuthProvider>
+      <SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Slot />
+        </GestureHandlerRootView>
+      </SafeAreaProvider>
+    </AuthProvider>
+  );
 }
